87842094cc29f18f92b5cb0f5e19ce735846c31e
Rea Antoniou-Kourounioti <rea.antkour@gmail.com>
2024-12-09 23:07:39 +0000

    Updates

---
+++ precache-manifest.0241919e3a4814bb94e683cf3393455a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48db6a15e0c1bd1ea794b7ed17e4bae4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6f6103e98aa17c448905",
+    "url": "/static/css/2.1cace5d8.chunk.css"
+  },
+  {
+    "revision": "910192d2a407eee07086",
+    "url": "/static/css/main.467e710e.chunk.css"
+  },
+  {
+    "revision": "6f6103e98aa17c448905",
+    "url": "/static/js/2.41137fe8.chunk.js"
+  },
+  {
+    "revision": "b889c04400af2a402f2934820106c4c2",
+    "url": "/static/js/2.41137fe8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "910192d2a407eee07086",
+    "url": "/static/js/main.6a8cb8dc.chunk.js"
+  },
+  {
+    "revision": "07ac36a7a63ec89df249",
+    "url": "/static/js/runtime-main.72f073c6.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.84609c9c8dbb0bfa41e2baf8bdaebfd1.js"
+  "/precache-manifest.0241919e3a4814bb94e683cf3393455a.js"
 );
 
 self.addEventListener('message', (event) => {
